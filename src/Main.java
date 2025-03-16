82172d9c172fa6ce9d718852aa54f62aaa048c87
Valera <valeramihalev628@gmail.com>
2025-03-16 18:26:55 +0300

    ten

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("gasa sagasa");
+        String s = "1234566778912345678912345678";
+        String formatted = s.replaceAll("(\\d)(?=(\\d{3})+$)", "$1,");
+        System.out.println(formatted);
     }
 }
