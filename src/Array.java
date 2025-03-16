a0b061e0f7e15c60a2a79b88d42f91369864d4e2
Valera <valeramihalev628@gmail.com>
2025-03-16 18:58:16 +0300

    new aray

--- src/Array.java
+++ src/Array.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Array {
     public static void main(String[] args) {
-        int[] array = {2, 5, 54, 7, 6446};
+        int[] array = {21, 52, 542, 73, 66};
         System.out.print(Arrays.toString(array));
     }
 }
