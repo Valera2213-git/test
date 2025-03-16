223b3317ac1ecafe807d32dc3752ba349e7220ea
Valera <valeramihalev628@gmail.com>
2025-03-16 18:54:48 +0300

    array

---
+++ src/Array.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] array = {2, 5, 54, 7, 6446};
+        System.out.print(Arrays.toString(array));
+    }
+}
